0b78b36f8d56cfb3f28d00999b8d52e3ea181f16
Helen_Abera <139908625+helenw4@users.noreply.github.com>
2025-02-10 17:17:20 -0500

    Update index.js

--- js/index.js
+++ js/index.js
@@ -7,7 +7,7 @@ body.appendChild(footer); // added node
 const today = new Date(); // variable stores/creates date object representing current date and time
 const year = today.getFullYear();
 const copyright = document.createElement("p");
-copyright.innerHTML = ` <span>&#169 ${year} Helen Abera</span>` // used temp literal to add in variables to create footer text back into HTML doc 
+copyright.innerHTML = ` <span>&copy; ${year} Helen Abera</span>`; // used temp literal to add in variables to create footer text back into HTML doc 
 footer.appendChild(copyright); 
 
 // area to add skiils items in section
